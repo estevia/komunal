594a5d84c9c8b8d29fb31a7297af2646069058e9
estevia <estevia.abellosi@lemonilo.com>
2023-02-15 17:06:42 +0700

    ok

--- cypress/plugins/index.ts
+++
@@ -1,9 +0,0 @@
-const { cypressBrowserPermissionsPlugin } = require('cypress-browser-permissions');
-const dotenvPlugin = require('cypress-dotenv');
-
-module.exports = (on, config) => {
-  config = cypressBrowserPermissionsPlugin(on, config);
-  config = dotenvPlugin(config,null,true);
-  return config
-}
-
--- cypress/support/page_objects/element/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as elementAddress } from './elementAddress';
-export { default as elementsNavigation } from './elementNavigation';

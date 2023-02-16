6f5be0d81976df9dcd307a6e8385d611b3cd19a2
estevia <estevia.abellosi@lemonilo.com>
2023-02-16 13:23:59 +0700

    ok

--- cypress/e2e/komunalsignin.cy.js
+++ cypress/e2e/komunalsignin.cy.js
@@ -2,10 +2,19 @@ const webUrl = 'https://user.depositobpr.id'
 
 const signin = () => {
     cy.visit(`${webUrl}/signin`)}
+    cy.get('[placeholder="Masukkan Alamat Email"]')
+    .type('abellosi97@gmail.com')
+ 
+    cy.get('[placeholder="Masukkan Kata Sandi"]')
+    .type('Estevi97')
+
+    cy.wait(100)
+    cy.get('[class="btn btn-primary btn-block btn-login mb-2"]')
+      .click()  
 
 
     describe('signin', () => {
         it('signin', () => {
-            signin();
+        signin();
         });
         });
